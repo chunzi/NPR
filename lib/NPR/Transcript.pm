996dcbbd631957635728be020f16aec89cdd17b4
chunzi <chunzi@gmail.com>
2017-07-25 18:38:45 +0800

    previous updates

--- lib/NPR/Transcript.pm
+++ lib/NPR/Transcript.pm
@@ -5,15 +5,17 @@ use XML::Tiny::Simple qw/ parsestring /;
 use Class::Date qw/ date /;
 use Text::Trim;
 use base qw(Class::Accessor);
-__PACKAGE__->mk_accessors(qw/
-    doc
-    npr_url published_at
-/);
+__PACKAGE__->mk_accessors(
+    qw/
+        doc
+        npr_url published_at
+        /
+);
 
-sub new { 
+sub new {
     my $class = shift;
-    my $id = shift;
-    my $self = bless {}, $class;
+    my $id    = shift;
+    my $self  = bless {}, $class;
 
     my $url = sprintf 'http://api.npr.org/transcript?id=%s&apiKey=MDExMTA1NDUwMDEzNjQyNjc4NDFmZjZkOA001', $id;
     my $res = HTTP::Tiny->new->get($url);
@@ -21,13 +23,13 @@ sub new {
 
     my $xml = $res->{content};
     my $doc = parsestring($xml);
-    $self->doc( $doc );
+    $self->doc($doc);
 
     my $npr_url = $doc->{transcript}{story}{link}[0]{content};
     my ( $yy, $mm, $dd, $sid, $slug ) = ( $npr_url =~ m{\.org/(.*?)/(.*?)/(.*?)/(.*?)/(.*)$} );
     my $published_at = date [$yy, $mm, $dd];
-    $self->npr_url( $npr_url );
-    $self->published_at( $published_at );
+    $self->npr_url($npr_url);
+    $self->published_at($published_at);
 
     return $self;
 }
@@ -38,29 +40,34 @@ sub paras {
 
     my @paras_new;
     my $speaker_previous = '';
-    for my $para ( @paras ){
-        my $speaker; 
-        my $words; 
+    for my $para (@paras) {
+        my $speaker;
+        my $words;
 
-        if ( $para =~ /:/ ){
-            ( $speaker, $words ) = map { trim } ( $para =~ /^([^a-z]*?):(.*)/ );
-            if ( not defined $speaker ){
+        if ( $para =~ /:/ ) {
+            ( $speaker, $words ) = map {trim} ( $para =~ /^([^a-z]*?):(.*)/ );
+            if ( not defined $speaker ) {
                 $speaker = $speaker_previous;
             }
             next if not defined $words;
 
-        }else{
+        }
+        else {
             $words = $para;
         }
 
-        my $background = ( $words =~ /^\(.*?\)$/ ) ? 1 : 0; 
+        my $background = ( $words =~ /^\(.*?\)$/ ) ? 1 : 0;
 
-        push @paras_new, { speaker => $speaker, words => $words, background => $background };
+        my @words = split /(\w+)/, $words;
+        my $awords = join '', map { /\w/ ? sprintf( q{<a href="#" class="word">%s</a>}, $_ ) : $_; } @words;
+
+        use Data::Dumper;
+        print STDERR Dumper $awords;
+
+        push @paras_new, { speaker => $speaker, words => $words, awords => $awords, background => $background };
     }
 
     return \@paras_new;
 }
-
-
 
 1;
