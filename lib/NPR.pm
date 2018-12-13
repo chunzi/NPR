219f4ec01b99b5248a300a04a41fe9b051e33a16
chunzi <chunzi@gmail.com>
2018-12-14 00:04:14 +0800

    more

--- lib/NPR.pm
+++ lib/NPR.pm
@@ -19,11 +19,16 @@ my $rss_dtf = DateTime::Format::RSS->new;
 #--------------------------------------------------------------------------------
 get '/' => sub {
 
-    # my $url = 'https://www.npr.org/rss/podcast.php';
-    # my $form  = { id => 510313 };
-    # my $html = $ua->get( $url, form => $form )->result->content;
+    my $file = ',podcast.xml';
 
-    my $html = read_file( ',podcast.xml' );
+    unless ( -f $file ){
+        my $url = 'https://www.npr.org/rss/podcast.php';
+        my $form  = { id => 510313 };
+        my $html = $ua->get( $url, form => $form )->result->body;
+        write_file( $file, $html );
+    }
+
+    my $html = read_file( $file );
 
     #----------------------------------------
     $rss->parse( $html );
@@ -102,12 +107,30 @@ get '/sid/:id' => sub {
     my $paras = $res->{paras};
     my @paras;
     for ( @$paras ) {
-        s{^([A-Z\s,]*?):}{<small class="text-info">$1:</small><br/>}sg;
-        push @paras, $_;
+        if ( /^([A-Z\s\,\.\-]*)?:/ ){
+            my ( $name, $para ) = split /\:/, $_, 2;
+            push @paras, { class => 'speaker', html => $name };
+            push @paras, { class => 'content', html => $para };
+
+        }elsif( /^\(.*?\)$/ ){
+            push @paras, { class => 'aside', html => $_ };
+        }else{
+            push @paras, { class => 'content', html => $_ };
+        }
+
+        # s{^([A-Z\s,]*?):}{<small class="text-info">$1:</small><br/>}sg;
+        # s{^(\(.*?\))$}{<small class="text-muted">$1</small>}sg;
+        # push @paras, $_;
     }
     pop @paras;
     pop @paras;
     var paras => \@paras;
+
+    for ( @paras ){
+        my $html = $_->{html};
+        my @words = split /(?![\w\'\-])/, $html;
+        $_->{words} = \@words;
+    }
 
     template 'story', vars;
 };
